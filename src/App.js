1694ea3f929482f4eb48e5b181fcb75792ac87e1
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-18 03:02:41 +0600

    derived state in useSelector

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="container is-fluid">
       <CarForm />
-      <CarList />
       <CarSearch />
+      <CarList />
       <CarValue />
     </div>
   );
