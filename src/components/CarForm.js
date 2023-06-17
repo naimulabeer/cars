9bad07fce03fe990caff5ffdbabc72d7bbe054c3
naimulabeer <naimul.h.abeer@gmail.com>
2023-06-18 02:10:11 +0600

    dispatching during the form submission

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { changeName, changeCost } from "../store";
+import { changeName, changeCost, addCar } from "../store";
 
 function CarForm() {
   const dispatch = useDispatch();
@@ -16,10 +16,15 @@ function CarForm() {
     dispatch(changeCost(carCost));
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(addCar({ name, cost }));
+  };
+
   return (
     <div className="car-form panel">
       <h4 className="subtitle is-3">Add Car</h4>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="field-group">
           <div className="field">
             <label className="label">Name</label>
@@ -40,6 +45,9 @@ function CarForm() {
               onChange={handleCostChange}
             />
           </div>
+        </div>
+        <div className="field">
+          <button className="button is-link">Submit</button>
         </div>
       </form>
     </div>
